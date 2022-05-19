3e2bbed88ba9f632283dd91ecce23dc69d4071d7
swanyee <swanyeetun.2000@gmail.com>
2022-05-19 23:12:01 +0630

    auth done

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -1,62 +1,110 @@
-<!DOCTYPE html>
-<html lang="en">
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Product | Category</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>{{ config('app.name', 'Laravel') }}</title>
+
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
+
+    <!-- Fonts -->
+    <link rel="dns-prefetch" href="//fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 </head>
 <body>
-    @if($errors->any())
-   @foreach ($errors->all() as $error)
-                    <script>
-                       alert('{{ $error }}')
-                    </script>
-    @endforeach
-@endif
+    <div id="app">
+        <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
+            <div class="container">
+                <a class="navbar-brand" href="{{ url('/') }}">
+                    {{ config('app.name', 'Laravel') }}
+                </a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
 
-@if(session()->has('success'))
-<script>
-    alert("{{ session()->get('success') }}")
- </script>
-@endif
-    {{-- header --}}
-    <nav class="navbar navbar-light bg-light">
-        <div class="container-fluid">
-            <a class="navbar-brand" href="#">Product | Category</a>
-        </div>
-    </nav>
-    {{-- content --}}
-    <div class="container">
-        <div class="row">
-            <div class="col-2">
-                <ul class="list-group">
-                <li class="list-group-item">
-                    <a href="{{route('product.index')}}">
-                        Product
-                    </a>
-                </li>
-                <li class="list-group-item">
-                    <a href="{{route('sub-category.index')}}">
-                        Sub Category
-                    </a>
-                </li>
-                <li class="list-group-item">
-                    <a href="{{route('category.index')}}">
-                        Category
-                    </a>
-                </li>
-                </ul>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <!-- Left Side Of Navbar -->
+                    <ul class="navbar-nav me-auto">
+
+                    </ul>
+
+                    <!-- Right Side Of Navbar -->
+                    <ul class="navbar-nav ms-auto">
+                        <!-- Authentication Links -->
+                        @guest
+                            @if (Route::has('login'))
+                                <li class="nav-item">
+                                    <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+                                </li>
+                            @endif
+
+                            @if (Route::has('register'))
+                                <li class="nav-item">
+                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+                                </li>
+                            @endif
+                        @else
+                            <li class="nav-item dropdown">
+                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                    {{ Auth::user()->name }}
+                                </a>
+
+                                <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
+                                    <a class="dropdown-item" href="{{ route('logout') }}"
+                                       onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                                        {{ __('Logout') }}
+                                    </a>
+
+                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                                        @csrf
+                                    </form>
+                                </div>
+                            </li>
+                        @endguest
+                    </ul>
+                </div>
             </div>
-            <div class="col-10">
-                @yield('content')
+        </nav>
+
+        <main class="py-4">
+            <div class="container">
+                <div class="row">
+                    @auth
+                    <div class="col-2">
+                            <ul class="list-group">
+                            <li class="list-group-item">
+                                <a href="{{route('product.index')}}">
+                                    Product
+                                </a>
+                            </li>
+                            <li class="list-group-item">
+                                <a href="{{route('sub-category.index')}}">
+                                    Sub Category
+                                </a>
+                            </li>
+                            <li class="list-group-item">
+                                <a href="{{route('category.index')}}">
+                                    Category
+                                </a>
+                            </li>
+                            </ul>
+                        </div>
+                        @endauth
+                        <div class="col-10">
+                            @yield('content')
+                        </div>
+                </div>
             </div>
-        </div>
+        </main>
     </div>
 </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -3,9 +3,17 @@
 use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\ProductController;
 use App\Http\Controllers\SubCategoryController;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
-Route::redirect('/','/product');
-Route::resource('/product', ProductController::class);
-Route::resource('/category',CategoryController::class);
-Route::resource('/sub-category',SubCategoryController::class);
+
+Route::group(['middleware' => ['auth']],function(){
+    Route::redirect('/','/product');
+    Route::resource('/product', ProductController::class);
+    Route::resource('/category',CategoryController::class);
+    Route::resource('/sub-category',SubCategoryController::class);
+});
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
